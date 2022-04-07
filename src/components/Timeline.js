f62494d99cbc71ccca9e02491eccf0c5efd043fe
Ruslan Khait <rusty6916@gmail.com>
2022-04-07 18:02:02 -0400

    Post Request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react'
+
 const Timeline= () => {
+    const [posts, setPost] = useState([])
+
+    useEffect( async() => {
+        console.log("Rendering Timeline...")
+        let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+        let res = req.json()
+        setPost(res)
+    }, [])
+
     return(
         <div>
-            <h2>Timeline</h2>
+            <h2>Timeline</h2>    
         </div>
     )
 }
