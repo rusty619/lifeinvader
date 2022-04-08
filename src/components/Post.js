94a722041246f8bcc6709e20a5c7c4b62264ae70
Ruslan Khait <rusty6916@gmail.com>
2022-04-08 13:44:47 -0400

    add like button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,14 @@
-
+import { useState } from "react";
    
 const Post = ({post}) => {
+
+    const [likes, SetLikes] = useState(0)
+
   return(
     <div>
       <img src={post.image} height="250" width="250" />
-      <p>Post by {post.username}</p>
+      <p>Post by {post.username} • {likes} likes</p>
+      <button onClick={() => {SetLikes(likes+1)}}>♥︎</button>
       <p>{post.content}</p>
     </div>
   )
